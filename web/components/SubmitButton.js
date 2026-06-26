460d32b13d951d71076e9c6f80cec99ad4ac8810
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-06-26 13:15:53 -0400

    Disable submit until required photos exist

--- web/components/SubmitButton.js
+++ web/components/SubmitButton.js
@@ -1,7 +1,11 @@
 // web/components/SubmitButton.js
-export default function SubmitButton({ loading, onClick }) {
+export default function SubmitButton({ loading, disabled, onClick }) {
   return (
-    <button className="submit" disabled={loading} onClick={onClick}>
+    <button
+      className="submit"
+      disabled={loading || disabled}
+      onClick={onClick}
+    >
       {loading ? 'Validando...' : 'Enviar informe'}
     </button>
   );
