35307d0cf819f5a519af62dbff9978ef299bc04d
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-06-27 00:37:58 -0400

    Add batch extraction note

---
+++ benchmark/scripts/run_batch_extract.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+console.log("Usar por ahora estos comandos por OT:");
+console.log("node benchmark/scripts/pdf_to_images.js <input.pdf> benchmark/tmp/<ot>.jpg");
+console.log("node benchmark/scripts/run_extract.js benchmark/tmp/<ot>.jpg <ot> <carpeta_destino>");
