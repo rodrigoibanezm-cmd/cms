ff5dd62ada3797399445711c6c23cafebe1eca16
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-10 09:46:44 -0400

    Explain upload readiness on submit

--- web/components/SubmitButton.js
+++ web/components/SubmitButton.js
@@ -1,12 +1,20 @@
-// web/components/SubmitButton.js
-export default function SubmitButton({ loading, disabled, onClick }) {
+export default function SubmitButton({ loading, disabled, missing, onClick }) {
   return (
-    <button
-      className="submit"
-      disabled={loading || disabled}
-      onClick={onClick}
-    >
-      {loading ? 'Validando...' : 'Enviar informe'}
-    </button>
+    <div className="submitArea">
+      <button
+        className="submit"
+        disabled={loading || disabled}
+        onClick={onClick}
+      >
+        {loading ? 'Enviando informe…' : 'Enviar informe'}
+      </button>
+      <p className={'submitHint ' + (!disabled ? 'submitReady' : '')}>
+        {loading
+          ? 'No cierres esta pantalla.'
+          : disabled
+            ? missing
+            : 'Todo listo para enviar'}
+      </p>
+    </div>
   );
 }
