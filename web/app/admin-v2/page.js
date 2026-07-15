893f4005d495f86488f31c9c9043f3eb1670ca04
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-15 09:50:37 -0400

    Build admin v2 foundation

--- web/app/admin-v2/page.js
+++ web/app/admin-v2/page.js
@@ -1,7 +1,11 @@
-import { headers } from 'next/headers';
-import OperationFooter from '../../components/admin/OperationFooter.js';
-import OperationHeader from '../../components/admin/OperationHeader.js';
-import OperationSummary from '../../components/admin/OperationSummary.js';
-import OperationTable from '../../components/admin/OperationTable.js';
-import ProcessingRefresh from '../../components/admin/ProcessingRefresh.js';
-import { listReports } from '../../lib/report
+import AdminV2View from '../../components/admin-v2/AdminV2View.js';
+import { loadAdminV2Data } from '../../helpers/admin_v2_helpers.js';
+
+export const dynamic = 'force-dynamic';
+
+export default async function AdminV2Page({ searchParams }) {
+  const params = await searchParams;
+  const data = await loadAdminV2Data(params);
+
+  return <AdminV2View {...data} />;
+}
