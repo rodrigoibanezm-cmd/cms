7b93a03fff1152facb4d1f5a4ab67a755e413999
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-07-15 09:32:52 -0400

    Add isolated admin v2 endpoint scaffold

---
+++ web/app/admin-v2/page.js
@@ -0,0 +1,7 @@
+import { headers } from 'next/headers';
+import OperationFooter from '../../components/admin/OperationFooter.js';
+import OperationHeader from '../../components/admin/OperationHeader.js';
+import OperationSummary from '../../components/admin/OperationSummary.js';
+import OperationTable from '../../components/admin/OperationTable.js';
+import ProcessingRefresh from '../../components/admin/ProcessingRefresh.js';
+import { listReports } from '../../lib/report
